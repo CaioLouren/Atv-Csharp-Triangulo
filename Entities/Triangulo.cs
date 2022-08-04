6ceb2d23d3964ba5d2af7a884b7f544d68506d62
Caio Lourenço <lourencocaio228@gmail.com>
2022-08-04 14:55:44 -0300

    Calculo da area do triangulo

---
+++ Entities/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.Entities
+{
+    class Triangulo
+    {
+        public double LadoA { get; set; }
+        public double LadoB { get; set; }
+        public double LadoC { get; set; }
+
+        public Triangulo(double ladoA, double ladoB, double ladoC)
+        {
+            LadoA = ladoA;
+            LadoB = ladoB;
+            LadoC = ladoC;
+        }
+
+        public double CalculoArea(Triangulo triangulo)
+        {
+            double p = (triangulo.LadoA + triangulo.LadoB + triangulo.LadoC) / 2;
+            double area = Math.Sqrt(p * (p - triangulo.LadoA) * (p - triangulo.LadoB) * (p - triangulo.LadoC));
+            return area;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp4.Entities;
+using System;
+using System.Globalization;
+
+namespace ConsoleApp4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE O VALOR DE CADA LADO: ");
+            Console.Write("LADO A: ");
+            double ladoA = double.Parse(Console.ReadLine());
+            Console.Write("LADO B: ");
+            double ladoB = double.Parse(Console.ReadLine());
+            Console.Write("LADO C: ");
+            double ladoC = double.Parse(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(ladoA, ladoB, ladoC);
+
+            double area = triangulo.CalculoArea(triangulo);
+            Console.WriteLine("A ÁREA DO TRIÂNGULO É:" + area);
+
+        }
+    }
+}
